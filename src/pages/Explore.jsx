87252cb5c140871f9955391fc5a6c6f2944e9998
Laurenz-Guevara <laurenzguevara@outlook.com>
2022-05-23 05:55:28 +0100

    Added Maquee

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import { React, useEffect, useRef } from 'react'
 import Footer from '../components/Footer'
+import '../styles/components/explore.scss'
 
 export default function Explore() {
+  let words = ['Apple', 'Banana', 'Cake', 'Donut', 'Egg', 'Fennel']
+
   return (
     <>
-      <h1>Explore Page</h1>
+      <div className="infinite-marquee wrapper">
+        <maquee className="danmaku" scrollamount="1">
+          <h1>Apple</h1>
+          <h1>Banana</h1>
+          <h1>Cake</h1>
+          <h1>Donut</h1>
+          <h1>Egg</h1>
+          <h1>Fennel</h1>
+        </maquee>
+      </div>
       <Footer />
     </>
   )
